8692bc15683702db734c9bcf1814c77a7457dab3
Bram Inniger <bram@inniger.be>
2023-01-14 01:07:00 +0100

    D20P02 - Solved

--- src/main/kotlin/be/inniger/advent/Day20.kt
+++ src/main/kotlin/be/inniger/advent/Day20.kt
@@ -4,42 +4,47 @@ object Day20 {
 
     private val groveNumbers = listOf(1_000, 2_000, 3_000)
 
-    fun solveFirst(file: List<Int>): Int {
+    fun solveFirst(file: List<Long>) = mix(file, nrRounds = 1, decryptionKey = 1)
+    fun solveSecond(file: List<Long>) = mix(file, nrRounds = 10, decryptionKey = 811_589_153)
+
+    private fun mix(file: List<Long>, nrRounds: Int, decryptionKey: Long): Long {
         // Set up and link Nodes in a bidirectional linked list
-        val nodes = file.map { Node(it) }
+        val nodes = file.map { Node(it * decryptionKey) }
         nodes.forEachIndexed { index, node ->
             node.prev = nodes[(index - 1).mod(nodes.size)]
             node.next = nodes[(index + 1).mod(nodes.size)]
         }
 
-        // Take every node in the order of original input
-        for (node in nodes) {
-            // Remove node from the circular list
-            node.prev!!.next = node.next
-            node.next!!.prev = node.prev
+        repeat(nrRounds) {
+            // Take every node in the order of original input
+            for (node in nodes) {
+                // Remove node from the circular list
+                node.prev!!.next = node.next
+                node.next!!.prev = node.prev
 
-            // Move the node to the new position
-            var newPosition = node
-            when {
-                node.value > 0 -> repeat(node.value) { newPosition = newPosition.next!! }
-                node.value <= 0 -> repeat(-node.value + 1) { newPosition = newPosition.prev!! }
+                // Move the node to the new position
+                var newPosition = node
+                (node.value % (nodes.size - 1)).toInt().let { nrMoves ->
+                    if (nrMoves > 0) repeat(nrMoves) { newPosition = newPosition.next!! }
+                    else repeat(-nrMoves + 1) { newPosition = newPosition.prev!! }
+                }
+
+                // Re-insert it
+                node.prev = newPosition
+                node.next = newPosition.next
+                node.prev!!.next = node
+                node.next!!.prev = node
             }
-
-            // Re-insert it
-            node.prev = newPosition
-            node.next = newPosition.next
-            node.prev!!.next = node
-            node.next!!.prev = node
         }
 
         // Find the 1000th, 2000th and 3000th element after 0 and sum them
         return groveNumbers.sumOf { findNthElement(nodes, it) }
     }
 
-//    fun solveSecond(file: List<Int>) = 42
+    private fun findNthElement(nodes: List<Node>, n: Int): Long {
+        val zero = nodes.single { it.value == 0L }
 
-    private fun findNthElement(nodes: List<Node>, n: Int): Int {
-        var node = nodes.single { it.value == 0 }
+        var node = zero
         repeat(n % nodes.size) { node = node.next!! }
 
         return node.value
@@ -47,7 +52,7 @@ object Day20 {
 
     @Suppress("unused")
     private fun List<Node>.print() {
-        val zero = single { it.value == 0 }
+        val zero = single { it.value == 0L }
         val buffer = StringBuilder()
 
         var current = zero
@@ -59,5 +64,5 @@ object Day20 {
         println(buffer)
     }
 
-    private data class Node(val value: Int, var prev: Node? = null, var next: Node? = null)
+    private data class Node(val value: Long, var prev: Node? = null, var next: Node? = null)
 }
--- src/test/kotlin/be/inniger/advent/Day20Test.kt
+++ src/test/kotlin/be/inniger/advent/Day20Test.kt
@@ -6,8 +6,8 @@ import kotlin.test.assertEquals
 
 class Day20Test {
 
-    private val input = readInputFile("20").map { it.toInt() }
-    private val sampleFile = listOf(1, 2, -3, 3, -2, 0, 4)
+    private val input = readInputFile("20").map { it.toLong() }
+    private val sampleFile = listOf<Long>(1, 2, -3, 3, -2, 0, 4)
 
     @Test
     fun validateFirstSampleInputs() {
@@ -19,13 +19,13 @@ class Day20Test {
         assertEquals(8_721, Day20.solveFirst(input))
     }
 
-//    @Test
-//    fun validateSecondSampleInputs() {
-//        assertEquals(0, Day20.solveSecond(sampleFile))
-//    }
-//
-//    @Test
-//    fun validateSecondSolution() {
-//        assertEquals(0, Day20.solveSecond(input))
-//    }
+    @Test
+    fun validateSecondSampleInputs() {
+        assertEquals(1_623_178_306, Day20.solveSecond(sampleFile))
+    }
+
+    @Test
+    fun validateSecondSolution() {
+        assertEquals(831_878_881_825, Day20.solveSecond(input))
+    }
 }
